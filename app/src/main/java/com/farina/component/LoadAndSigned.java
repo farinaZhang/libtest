5e3b73eccb103ee34d4d4982e273c2e8b1b9cb36
FarinaZhang <zhangxy0605@126.com>
2016-05-25 15:32:41 +0800

    添加登录注册画面

---
+++ app/src/main/java/com/farina/component/LoadAndSigned.java
@@ -0,0 +1,33 @@
+package com.farina.component;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class LoadAndSigned extends View {
+
+
+    public LoadAndSigned(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public LoadAndSigned(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public LoadAndSigned(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+
+    }
+
+}
---
+++ app/src/main/java/com/farina/data/UserData.java
@@ -0,0 +1,15 @@
+package com.farina.data;
+
+import android.content.Context;
+
+public class UserData{
+    Context context;
+    Boolean isLoad;
+
+
+    public UserData(Context context){
+        this.context = context;
+    }
+
+
+}
