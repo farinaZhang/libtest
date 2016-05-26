3f8fabc34a392b0f6e657feb3e89c6300ea63442
FarinaZhang <zhangxy0605@126.com>
2016-05-26 17:27:35 +0800

    添加登录注册画面,CircleImageView.jar

--- app/src/main/java/com/farina/component/LoadAndSigned.java
+++ app/src/main/java/com/farina/component/LoadAndSigned.java
@@ -1,33 +1,48 @@
 package com.farina.component;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import com.farina.farinaimagelib.CircleImageView;
+import com.farina.libtest.R;
 
 /**
  * TODO: document your custom view class.
  */
-public class LoadAndSigned extends View {
-
+public class LoadAndSigned extends LinearLayout {
+    private Context context;
+    private CircleImageView userImg;
 
     public LoadAndSigned(Context context) {
         super(context);
-        init(null, 0);
+        init(context,null, 0);
     }
 
     public LoadAndSigned(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs, 0);
+        init(context,attrs, 0);
     }
 
     public LoadAndSigned(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init(attrs, defStyle);
+        init(context,attrs, defStyle);
     }
 
-    private void init(AttributeSet attrs, int defStyle) {
+    private void init(Context context,AttributeSet attrs, int defStyle) {
         // Load attributes
-//asd
+        this.context = context;
+        View view = LayoutInflater.from(getContext()).inflate(
+                R.layout.load_and_signed, null);
+
+        addView(view);
+
+        userImg = (CircleImageView)view.findViewById(R.id.user_img);
+        userImg.setBorderWidth(5);
+        userImg.setBorderColor(Color.parseColor("#ff0000"));
     }
 
 }
